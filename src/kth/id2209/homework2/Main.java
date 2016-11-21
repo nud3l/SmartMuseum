cbdcf5a878dab0f7dd257f7e03744a4bd46b7328
nud3l <dominik.harz@gmail.com>
2016-11-21 18:30:41 +0100

    Added auction object and create ArtistManagementAgent

---
+++ src/kth/id2209/homework2/Main.java
@@ -0,0 +1,7 @@
+package kth.id2209.homework2;
+
+/**
+ * Created by nud3l on 11/21/16.
+ */
+public class Main {
+}
---
+++ src/kth/id2209/homework2/agent/ArtistManagementAgent.java
@@ -0,0 +1,7 @@
+package kth.id2209.homework2.agent;
+
+/**
+ * Created by nud3l on 11/21/16.
+ */
+public class ArtistManagementAgent {
+}
